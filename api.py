e16a57a6c8e501d7ad0f800dcda400d4a71af483
dhruv.shah <hawksterdhruv@gmail.com>
2023-05-26 02:00:44 +0530

    Working index file, Results api

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 import json
 from typing import Dict, List
-from flask import Flask, jsonify, request
-from models import Student, Course
+from flask import Flask, jsonify, render_template, request
+from models import Student, Course, Result
+
 # from schemas import StudentSchema
 from db import db
+
 # from serializer import ma
 
 app = Flask(__name__)
@@ -16,14 +18,20 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return jsonify({"data": "OK"})
+    students: List[Dict] = [student.to_dict() for student in Student.query.all()]
+    courses: List[Dict] = [course.to_dict() for course in Course.query.all()]
+    results: List[Dict] = [result.to_dict() for result in Result.query.all()]
+    data = {"students": students, "courses": courses, "results": results}
+    return render_template("index.html", data=data)
+
+
+##################################
 
 
 @app.route("/api/v1/students")
 def students():
     students_results: List[Student] = Student.query.all()
-    student_list: List[Dict] = [student.to_dict()
-                                for student in students_results]
+    student_list: List[Dict] = [student.to_dict() for student in students_results]
 
     json.dumps({"data": student_list, "message": "SUCCESS"})
     # students_schema = StudentSchema()
@@ -38,7 +46,6 @@ def get_student(id):
 
 @app.route("/api/v1/student/", methods=["POST"])
 def add_student():
-
     student_raw = request.get_json()
     new_student = Student().from_dict(student_raw)
 
@@ -58,11 +65,11 @@ def delete_student(id):
 
 ##################################
 
+
 @app.route("/api/v1/courses")
 def courses():
     courses_results: List[Course] = Course.query.all()
-    course_list: List[Dict] = [course.to_dict()
-                                for course in courses_results]
+    course_list: List[Dict] = [course.to_dict() for course in courses_results]
 
     json.dumps({"data": course_list, "message": "SUCCESS"})
     # courses_schema = courseSchema()
@@ -77,7 +84,6 @@ def get_course(id):
 
 @app.route("/api/v1/course/", methods=["POST"])
 def add_course():
-
     course_raw = request.get_json()
     new_course = Course().from_dict(course_raw)
 
@@ -93,6 +99,45 @@ def delete_course(id):
     db.session.delete(course)
     db.session.commit()
     return jsonify({"data": {}, "message": "SUCCESS"})
+
+
+##################################
+
+
+@app.route("/api/v1/results")
+def results():
+    results_results: List[Result] = Result.query.all()
+    result_list: List[Dict] = [result.to_dict() for result in results_results]
+
+    json.dumps({"data": result_list, "message": "SUCCESS"})
+    # results_schema = resultSchema()
+    return jsonify({"data": result_list, "message": "SUCCESS"})
+
+
+@app.route("/api/v1/result/<int:id>", methods=["GET"])
+def get_result(id):
+    result = Result.query.get_or_404(id)
+    return jsonify({"data": result.to_dict(), "message": "SUCCESS"})
+
+
+@app.route("/api/v1/result/", methods=["POST"])
+def add_result():
+    result_raw = request.get_json()
+    new_result = Result().from_dict(result_raw)
+
+    # Todo: Add validation here or check validation here
+    db.session.add(new_result)
+    db.session.commit()
+    return jsonify({"data": new_result.to_dict(), "message": "SUCCESS"})
+
+
+@app.route("/api/v1/result/<int:id>", methods=["DELETE"])
+def delete_result(id):
+    result = Result.query.get_or_404(id)
+    db.session.delete(result)
+    db.session.commit()
+    return jsonify({"data": {}, "message": "SUCCESS"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -27,7 +27,8 @@ class Student(db.Model):
         instance.first_name = raw_student['first_name']
         instance.family_name = raw_student['family_name']
         instance.email = raw_student['email']
-        instance.date_of_birth = date.fromisoformat(raw_student['date_of_birth'])
+        instance.date_of_birth = date.fromisoformat(
+            raw_student['date_of_birth'])
         return instance
 
 
@@ -35,10 +36,40 @@ class Course(db.Model):
     id = sa.Column(sa.Integer, primary_key=True)
     course_name = sa.Column(sa.String, nullable=False)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "course_name": self.course_name
+        }
+
+    @classmethod
+    def from_dict(cls, raw_course):
+        instance = cls()
+        instance.course_name = raw_course['course_name']
+
+        return instance
+
 
 class Result(db.Model):
     id = sa.Column(sa.Integer, primary_key=True)
-    student_id = sa.ForeignKey(Student.id)
-    course_id = sa.ForeignKey(Course.id)
+    student_id = sa.Column(sa.ForeignKey(Student.id), nullable=False)
+    course_id = sa.Column(sa.ForeignKey(Course.id), nullable=False)
     # Todo : Change grade to enum instead of String
     grade = sa.Column(sa.String, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "student_id": self.student_id,
+            "course_id": self.course_id,
+            "grade": self.grade,
+        }
+
+    @classmethod
+    def from_dict(cls, raw_result):
+        instance = cls()
+        instance.student_id = raw_result['student_id']
+        instance.course_id = raw_result['course_id']
+        instance.grade = raw_result['grade']
+
+        return instance
